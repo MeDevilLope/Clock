31b8ea5ae70c2844340f723fae477ee8e55c7994
Mario Giessler <53855228+mariogie@users.noreply.github.com>
2019-09-02 10:55:27 +0200

    Update Clock.java

--- src/Clock.java
+++ src/Clock.java
@@ -1,6 +1,8 @@
 /*
  * Simple 7-Segment Clock
- * Developed Sep. 9th 2019 by M. Giessler
+ * Developed Sep. 9th 2019 (c) M. Giessler
+ *
+ * Please ask for commercial use and Modifications!
  */
 public class Clock {
  
